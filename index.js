fcf97cb3b07d33946361e62b1b44e884b230a383
tanvir1017 <tanvir.hossaindev1@gmail.com>
2022-02-09 00:38:54 +0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const ObjectId = require("mongodb").ObjectId;
 const app = express();
 require("dotenv").config();
-const port = process.env.PROT || 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
